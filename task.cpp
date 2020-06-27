092c818afabac3f24c8a00a74fc737097c03d7e3
Shutarou_Motohashi <motohashi_syu512@fuji.waseda.jp>
2020-06-27 22:47:52 +0900

    create task.cpp

---
+++ task.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    int a = 3;
+    int b = 5;
+
+    printf("%d\n",a);
+    printf("%d\n",b);
+
+    printf("%d\n",a+b);
+
+    return 0;
+}
